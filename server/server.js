e77431470e1430cfaaff7c4637ea9867574317dd
Vineet Dandekar <vineet_ld@live.com>
2017-09-26 18:04:28 -0400

    Adds a method to GET a todo with an ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var {ObjectID} = require("mongodb");
 
 var {mongoose} = require("./db/mongoose");
 var {User} = require("./models/user");
@@ -26,6 +27,27 @@ app.get("/todos", (request, response) => {
     Todo.find()
         .then((todos) => response.send({todos}))
         .catch((e) => response.status(400).send(e));
+});
+
+app.get("/todos/:id", (request, response) => {
+
+    let id = request.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return response.status(400).send({
+            error: "Invalid ID"
+        });
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return response.status(404).send({
+                error: "Todo not found"
+            });
+        }
+        response.send(todo);
+    }).catch((e) => response.status(400).send(e));
+
 });
 
 app.listen(port, () => {
