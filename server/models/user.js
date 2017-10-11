1867b71e1ac68063ac0d0d57620bbd6da18a0878
Vineet Dandekar <vineet_ld@live.com>
2017-10-11 16:05:17 -0400

    Hashes passwords before persisting them

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require("lodash");
+const bcrypt = require("bcryptjs");
 
 var UserSchema = new mongoose.Schema({
     email: {
@@ -76,6 +77,25 @@ UserSchema.statics.findByToken = function(token) {
     });
 
 };
+
+UserSchema.pre("save", function(next) {
+    var user = this;
+
+    if(user.isModified("password")) {
+        bcrypt.genSalt(10, (error, salt) => {
+            if(!error) {
+                bcrypt.hash(user.password, salt, (err, hash) => {
+                    if(!error) {
+                        user.password = hash;
+                        next();
+                    }
+                });
+            }
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model("User", UserSchema);
 
